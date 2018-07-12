9a0461095813023a30aec1eec2d1f6292eacda4e
jrhoads <joseph_rhoads@brown.edu>
2018-07-12 10:45:53 -0400

    Break build_site.sh into smaller functions.  Switch to git for downloading master texts

--- build_site.sh
+++ build_site.sh
@@ -3,22 +3,35 @@
 update=1;
 exceptions=0;
 use_existing=0;
+text_path=iip_master_texts
+xml_text_path=$text_path/epidoc-files;
 #new_system=0;
 
 run_script() {
-	source environment/bin/activate;
 	cd docs;
 	exceptions_flag=""
 	new_system_flag=""
 	if [ $exceptions == 1 ]; then
 		exceptions_flag="--fileexception"
 	fi
+
+	echo "Running Script...";
+	../src/python/wordlist.py ../$xml_text_path/* --nodiplomatic --html_general\
+	--plaintext --flat texts/plain $exceptions_flag $new_system_flag;
 	#if [ $new_system == 1 ]; then
 	#	new_system_flag="--new_system"
 	#fi
-	../src/python/wordlist.py texts/xml/* --nodiplomatic --html_general\
-	--plaintext --flat texts/plain $exceptions_flag $new_system_flag;
 	cd ..;
+}
+copy_static(){
+	echo "Copying Static Assets...";
+	cp src/web/wordlist.css docs/;
+	cp src/web/style.css docs/;
+	cp src/web/index_search.js docs/;
+	cp src/web/doubletree.html docs/;
+	cp -r src/web/doubletreejs docs/;
+	cp src/web/levenshtein.min.js docs/;
+	cp src/web/wordinfo.css docs/;
 }
 
 for word in $*; do 
@@ -41,49 +54,50 @@ for word in $*; do
 	
 done
 
-echo "Removing old site...";
-if [ -d docs ]; then
-	cd docs;
-	if [ $update == 0 ]; then
-		mv texts ..;
+reset_docs(){
+	echo "Removing old site...";
+	if [ -d docs ]; then
+		rm -rf docs
 	fi
-	cd ..;
-	rm -rf docs
-fi
-mkdir docs
+	mkdir docs
+}
+
+process_doubletree(){
+	echo "Processing doubletree-data...";
+	cat docs/texts/plain/* > docs/combined.txt
+	./src/python/per_line.py docs/combined.txt docs/doubletree-data.txt
+}
 
 if [ $update == 1 ]; then
 	echo "Updating texts...";
-	mkdir temp;
-	cd temp;
-	wget $(echo "https://github.com/Brown-University-Library/iip-texts/\
-	archive/master.zip" | sed -e 's:\t::g');
-	unzip master.zip;
-	mkdir ../docs/texts;
-	cp -r iip-texts-master/epidoc-files/ ../docs/texts/xml;
-	cd ..;
-	rm -rf temp;
-	cd docs/texts/xml;
+	if [ -d $text_path ]; then
+		cd $text_path;
+		git pull;
+		cd - > /dev/null;
+	else
+		git clone --depth=1 https://github.com/Brown-University-Library/iip-texts.git $text_path
+	fi
+
+	cd $xml_text_path;
 	if [ -f interpretations.xml ]; then
 		rm interpretations.xml;
 	fi
 	if [ -f include_publicationStmt.xml ]; then
 		rm include_publicationStmt.xml;
 	fi
-	cd ../../..;
-else
-	mv texts docs;
+	cd - > /dev/null;
 fi
 
+reset_docs;
+copy_static;
 run_script;
+process_doubletree;
 
-cp src/web/wordlist.css docs/;
-cp src/web/style.css docs/;
-cp src/web/index_search.js docs/;
-cp src/web/doubletree.html docs/;
-cp -r src/web/doubletreejs docs/;
-cp src/web/levenshtein.min.js docs/;
-cp src/web/wordinfo.css docs/;
+#cp src/web/wordlist.css docs/;
+#cp src/web/style.css docs/;
+#cp src/web/index_search.js docs/;
+#cp src/web/doubletree.html docs/;
+#cp -r src/web/doubletreejs docs/;
+#cp src/web/levenshtein.min.js docs/;
+#cp src/web/wordinfo.css docs/;
 
-cat docs/texts/plain/* > docs/combined.txt
-./src/python/per_line.py docs/combined.txt docs/doubletree-data.txt
